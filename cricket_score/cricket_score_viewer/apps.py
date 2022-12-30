7383ddf18b2f51abe66b4f0de3b32226769f53f0
Sahil <lunawatsahil321@gmail.com>
2022-12-30 16:27:31 +0530

    Added no ball feature

--- cricket_score/cricket_score_viewer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CricketScoreViewerConfig(AppConfig):
-    name = 'cricket_score_viewer'
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var ball_no = 1; // Ball number will start from 1
 var over_no = 1; // Over number will start from 1
 var runs = 0;
 var edited = [];
+var isNoBall = false;
+
 $(document).ready(function () {
 	// window.onresize = function(event) {
 	// 	vpw = $(window).width();
@@ -23,6 +25,9 @@ $(document).ready(function () {
 	});
 	$("#run_wide").on("click", function (event) {
 		play_ball("+", 0);
+	});
+	$("#run_no_ball").on("click", function (event) {
+		play_ball("NB", 0);
 	});
 	$("#run_4").on("click", function (event) {
 		play_ball(4);
@@ -46,27 +51,42 @@ function play_ball(run, score = 1) {
 		update_score();
 		return;
 	}
+	if (run == "NB") {
+		isNoBall = true;
+		//No ball
+		runs++;
+		scoreboard[over_no][0] += 1;
+		update_score();
+		return;
+	}
 	if (score == 1) {
 		runs += run;
 	}
-	console.log("over_no=", over_no, "| ball_no=", ball_no," |Runs=",runs);
-	scoreboard[over_no][ball_no] = run;
-	console.log(scoreboard[over_no])
-	update_runboard();
-	ball_no++;
-	if (ball_no >= 7) {
-		ball_no = 1;
-		over_no++;
-		scoreboard[over_no] = [];
-		scoreboard[over_no][0] = 0; //Wide bowls counter
+	// console.log("over_no=", over_no, "| ball_no=", ball_no," |Runs=",runs);
+
+	if (isNoBall) {
+		scoreboard[over_no][0] += run;
+		update_score();
+		isNoBall = false;
+	} else {
+		scoreboard[over_no][ball_no] = run;
+		// console.log(scoreboard[over_no])
+		update_runboard();
+		ball_no++;
+		if (ball_no >= 7) {
+			ball_no = 1;
+			over_no++;
+			scoreboard[over_no] = [];
+			scoreboard[over_no][0] = 0; //Wide bowls counter
+		}
 	}
 	update_score();
 }
 
 function update_runboard() {
 	// Updates the runboard when the function is called
-	for(i=1;i<7;i++){
-		let score_und = (_score_und)=>_score_und==undefined?"":_score_und;
+	for (i = 1; i < 7; i++) {
+		let score_und = (_score_und) => (_score_und == undefined ? "" : _score_und);
 		$("#ball_no_" + i.toString()).html(score_und(scoreboard[over_no][i]));
 	}
 	if (ball_no != 1) {
@@ -78,21 +98,29 @@ function update_runboard() {
 			$("#ball_no_" + i.toString()).addClass("btn-light");
 		}
 	}
-	$("#over-ball").html((over_no-1).toString() + "." + ball_no.toString());
+	$("#over-ball").html((over_no - 1).toString() + "." + ball_no.toString());
 }
 
 function change_score() {
 	let over = parseInt($("#change_over").val());
 	let ball = parseInt($("#change_ball").val());
 	let run = parseInt($("#change_run").val());
-	edited.push([over,ball,scoreboard[over][ball],run]);
+	edited.push([over, ball, scoreboard[over][ball], run]);
 	scoreboard[over][ball] = run;
 	update_score();
 	update_scoreboard();
 	$("#run").html(runs);
 	let edited_scores = "Edited scores:<br>";
-	for(i=0; i<edited.length;i++){
-		edited_scores += "("+edited[i][0].toString()+"."+edited[i][1].toString()+") = " + edited[i][2].toString() + " -> " + edited[i][3].toString();
+	for (i = 0; i < edited.length; i++) {
+		edited_scores +=
+			"(" +
+			edited[i][0].toString() +
+			"." +
+			edited[i][1].toString() +
+			") = " +
+			edited[i][2].toString() +
+			" -> " +
+			edited[i][3].toString();
 		edited_scores += "<br>";
 	}
 	// }
@@ -120,16 +148,16 @@ function update_scoreboard() {
 function update_score() {
 	let score = 0;
 	for (i = 1; i <= over_no; i++) {
-		let numOr0 = (n) => (n=="+"?1:isNaN(n) ? 0 : n);
+		let numOr0 = (n) => (n == "+" ? 1 : isNaN(n) ? 0 : n);
 		score += scoreboard[i].reduce((a, b) => numOr0(a) + numOr0(b));
 	}
 	runs = score;
 	$("#run").html(runs);
 }
 
-function back_button(){
+function back_button() {
 	ball_no--;
-	if(ball_no==0){
+	if (ball_no == 0) {
 		ball_no = 6;
 		over_no--;
 	}
@@ -137,5 +165,7 @@ function back_button(){
 	update_score();
 	update_scoreboard();
 	update_runboard();
-	$("#over-ball").html((over_no-1).toString() + "." + (ball_no-1).toString());
+	$("#over-ball").html(
+		(over_no - 1).toString() + "." + (ball_no - 1).toString()
+	);
 }
